bcdb92240c72fc35ee2e5ac36b599207a87c82fe
Natanius18 <leonidchachko17@gmail.com>
2021-09-13 15:55:47 +0300

    int to long

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,36 +4,36 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(luckyTickets(8));
+        System.out.println(luckyTickets(18));
     }
 
-    public static int luckyTickets(int length) {
+    public static long luckyTickets(int length) {
         if (length % 2 != 0) return 0;
         if (length == 2) return 10;
 
-        int res = 0, tmpSum = 0;
-        int[] arrayNn = new int[(length / 2) * 9 + 1], arrayNnPrev;
+        long res = 0, tmpSum = 0;
+        long[] arrayNn = new long[(length / 2) * 9 + 1], arrayNnPrev;
 
-        for (int i = 0; i <= 9; i++) {
-            arrayNn[i] = 1;                 // заполнили единицами первые 10 элементов
+        for (long i = 0; i <= 9; i++) {
+            arrayNn[(int) i] = 1;                 // заполнили единицами первые 10 элементов
         }
 
         arrayNnPrev = Arrays.copyOf(arrayNn, arrayNn.length);
 
-        for (int n = 1; n < length / 2; n++) {
-            for (int k = 0; k <= length / 2 * 9; k++) {
-                for (int l = 0; l < 10; l++) {                  //  считаем сумму 10-ти чисел из предыдущего массива,
+        for (long n = 1; n < length / 2; n++) {
+            for (long k = 0; k <= length / 2 * 9L; k++) {
+                for (long l = 0; l < 10; l++) {                  //  считаем сумму 10-ти чисел из предыдущего массива,
                     if (l <= k) {                              //   получаем значение k-го элемента настоящего массива
-                        tmpSum += arrayNnPrev[k - l];
+                        tmpSum += arrayNnPrev[(int) (k - l)];
                     }
                 }
-                arrayNn[k] = tmpSum;
+                arrayNn[(int) k] = tmpSum;
                 tmpSum = 0;
             }
             arrayNnPrev = Arrays.copyOf(arrayNn, arrayNn.length);
         }
-        for (int i = 0; i <= 9 * length / 2; i++) {
-            res += arrayNn[i] * arrayNn[i];
+        for (long i = 0; i <= 9L * length / 2; i++) {
+            res += arrayNn[(int) i] * arrayNn[(int) i];
         }
         return res;
     }
